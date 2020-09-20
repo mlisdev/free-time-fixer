5c78faf4bef566c73fdf62ba23011933d9fbfc53
khillerb <64817143+khillerb@users.noreply.github.com>
2020-09-20 12:02:31 -0500

    Models

--- freetimefixer/main_app/models.py
+++ freetimefixer/main_app/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Activities(models.Model):
+    class TimeChoices(models.IntegerChoices):
+        FIFTEEN = 15 
+        HALFHOUR = 30
+        FOURTYFIVE = 45
+        HOUR = 60
+        HOURHALF = 90
+        TWOHOUR = 120
+    timeChoices = models.IntegerField(choices=TimeChoices.choices)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    category = models.CharField(max_length=100)
+    class Category(models.TextChoices):
+        WATCH = 'Watch'
+        READ = 'Read'
+        FITNESS = 'Fitness'
+        PLAY = 'Play'
+    category = models.CharField(choices=Category.choices)
+    finished = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
