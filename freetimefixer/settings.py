a7a2d26c504d8d66bae607f8ce53ecef66f32607
Stephanie Rinehart <rinehart@MacBook-Pro.local>
2020-09-23 13:24:14 -0400

    Config deployment

--- freetimefixer/settings.py
+++ freetimefixer/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +124,6 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/activities/'
 LOGOUT_REDIRECT_URL = '/'
+
+import django_heroku
+django_heroku.settings(locals())
