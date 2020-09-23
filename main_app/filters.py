72d871638b531c6ef88681e95c15fbe68557c234
khillerb <64817143+khillerb@users.noreply.github.com>
2020-09-23 16:02:47 -0500

    Attempt to fix dropdown

--- main_app/filters.py
+++ main_app/filters.py
@@ -2,14 +2,26 @@ from django.contrib.auth.models import User
 import django_filters
 from .models import Activity
 from django import forms 
+from django.utils.translation import ugettext as _
+
 
 class ActivityFilter(django_filters.FilterSet):
+    TIME_CHOICES = (
+        (15, _('15 Minutes')), 
+        (30, _('30 Mintues')), 
+        (45, _('45 Minutes')),
+        (60, _('1 Hour')), 
+        (90, _('1 Hour 30 Minutes')), 
+        (120, _('2 hours')), 
+    )
+    timeChoices = django_filters.ChoiceFilter(choices=TIME_CHOICES, widget=forms.Select(attrs={'class': 'form-control form-control-sm'}))
     class Meta:
         model = Activity
         fields = {
-            'category': ['exact'],
-            'timeChoices': ['lte']
+            'category': ['exact']
+            'timeChoices' : ['lte']
         }
+    
 
     
 
